603d83d848f8667e74d784a6d7c73a89c5797117
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2025-08-06 21:54:50 +0700

    Add Prisma seeding script and update package.json for seed command

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import prisma from "@/lib/prisma";
+import { Prisma } from "../app/generated/prisma";
+
+const eventData: Prisma.EventCreateInput[] = [
+  {
+    name: "PMC BATCH 3",
+    date: new Date(),
+    startTime: new Date(),
+    endTime: new Date(),
+    location: "Jakarta",
+  },
+];
+
+export async function main() {
+  for (const e of eventData) {
+    await prisma.event.create({ data: e });
+  }
+}
+
+main();
