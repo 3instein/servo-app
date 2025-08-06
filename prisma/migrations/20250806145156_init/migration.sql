15a7ab14137e77b3d62574259c9742b132327bea
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2025-08-06 21:52:47 +0700

    Add Prisma integration with schema and initial migration setup

---
+++ prisma/migrations/20250806145156_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."Event" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "endTime" TIMESTAMP(3) NOT NULL,
+    "location" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
